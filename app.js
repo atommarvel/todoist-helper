2ec8e2e7e3299a0631896af4df7bb8bd5f8f3061
Atom Raiff <atom.raiff@gmail.com>
2019-03-02 23:31:29 -0800

    stop registering index router

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/api');
 
 var app = express();
@@ -27,7 +26,6 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.use('/', indexRouter);
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
